d1266fa2cdc512ecc085db785fd90db4c9ba6d13
varunreddy <varunreddy6666@yahoo.com>
2016-06-03 19:55:58 +0530

    first day assignment

---
+++ AssignmentProject/src/assignments/Assignment1.java
@@ -0,0 +1,15 @@
+package assignments;
+public class Assignment1 {
+
+	public static void main(String[] args) 
+	{
+		int i=10;
+		float f=0.34f;
+		
+		int sum=(int)(10+20.05f);
+		
+		System.out.println(sum);
+
+	}
+
+}
